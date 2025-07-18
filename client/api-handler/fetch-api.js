1f58dde6395e5c3b33888bf87c123bd8b55f0eca
Mounish-T <tmounish02@gmail.com>
2025-07-18 14:47:16 +0530

    ip changed

--- client/api-handler/fetch-api.js
+++ client/api-handler/fetch-api.js
@@ -1,5 +1,5 @@
-const mainUrl = 'http://192.168.165.244:1000/';
-const baseUrl = 'http://192.168.165.244:1000/tasks';
+const mainUrl = 'http://192.168.167.244:1000/';
+const baseUrl = 'http://192.168.167.244:1000/tasks';
 
 export async function defaultRoute() {
     try{
